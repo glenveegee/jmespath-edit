273fec8c260a6582f3d1990d5c2b73e8d1f176b6
Glen Van Ginkel <glen.vanginkel@nanoporetech.com>
2020-07-10 19:24:06 +0100

    Type jmespath lib after dropping dependency

---
+++ src/typings/jmespath/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Take a JSON document and transform it into another JSON document
+ * through a JMESPath expression.  See: http://jmespath.org/
+ * @param jsonDoc the document to transform
+ * @param query a JMESPath expression
+ * @return the transformed document
+ */
+declare module 'jmespath' {
+  export function search(jsonDoc: any, query: string): any;
+}
